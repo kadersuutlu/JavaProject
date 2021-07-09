a7c4a2ec748896b00a1d629b95d82944f7aa59d1
kadersuutlu <ogr.k.sutlu34@ahievran.edu.tr>
2021-07-09 13:19:43 +0300

    düzenlendi

--- intro/src/intro/Main.java
+++ intro/src/intro/Main.java
@@ -3,8 +3,82 @@ package intro;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//camelCase
+		//Don't repeat yourself
+		String internetSubeButonu="Ýnternet Þubeye gir";
+		double dolarDun=8.15;
+		double dolarBugun=8.20;
+		int vade=36;
+		boolean dustuMu=false;
+		
+		System.out.println(internetSubeButonu);
 
+		if(dolarBugun<dolarDun) {
+			System.out.println("Dolar düþtü resmi");
+		}
+		else if(dolarBugun>dolarDun) {
+			System.out.println("Dolar yükseldi resmi");
+		}
+		else { //sýnýr analizi
+			System.out.println("Dolar eþittir resmi");
+		}
+		
+		String kredi1="Hýzlý Kredi";
+		String kredi2="Mutlu Emekli Kredisi";
+		String kredi3="Konut Kredisi";
+		String kredi4="Çiftçi Kredisi";
+		String kredi5="MSB Kredisi";
+		String kredi6="MEB Kredisi";
+		String kredi7="Kültür Bakanlýðý Kredisi";
+		
+		System.out.println(kredi1);
+		System.out.println(kredi2);
+		System.out.println(kredi3);
+		System.out.println(kredi4);
+		System.out.println(kredi5);
+		System.out.println(kredi6);
+		
+		String[] krediler= 
+			{
+				"Hýzlý Kredi",
+				"Mutlu Emekli Kredisi",
+				"Konut Kredisi",
+				"Çiftçi Kredisi",
+				"MSB Kredisi",
+				"MEB Kredisi",
+				"Kültür Bakanlýðý Kredisi"
+			};
+		
+		for(String kredi:krediler) {
+			System.out.println(kredi);
+		}
+		
+		for(int i=0;i<krediler.length;i++) {
+			System.out.println(krediler[i]);
+		}
+		
+		//ÝMPORTANT!!!!
+		
+		//value type: deðer tipler
+		int sayi1=10;
+		int sayi2=20;
+		sayi1=sayi2;
+		sayi2=100;
+		System.out.println(sayi1);//20
+		
+		//referance types: referans tipler
+		int[] sayilar1= {1,2,3,4,5};
+		int[] sayilar2= {10,20,30,40,50};
+		sayilar1=sayilar2;//sayilar1'in adresi=sayilar2'nin adresi
+		sayilar2[0]=100;
+		System.out.println(sayilar1[0]);//100
+		
+		//istisna: stringler array olsa da deðer tip olarak çalýþýr.
+		String sehir1="Ankara";
+		String sehir2="Ýstanbul";
+		sehir1=sehir2;
+		sehir2="Ýzmir";
+		System.out.println(sehir1);//Ýstanbul
 	}
 
 }
